294fd24d4b3ed633c8c09275165300e21e6b2b8d
Dimaskes <d.imon-million@yandex.ru>
2020-11-18 17:59:23 +0400

    Render tetromino and movements

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,13 +1,31 @@
-import { useState } from 'react';
+import { useState, useCallback} from 'react';
+import { STAGE_WIDTH } from '../gameHelpers';
 
-import { randomTetromino } from '../tetrominos';
+import { TETROMINOS, randomTetromino } from '../tetrominos';
 
 export const usePlayer = () => {
     const [player, setPlayer] = useState({
         position: { x: 0, y: 0 },
-        tetromino: randomTetromino().shape, 
+        tetromino: TETROMINOS[0].shape, 
         collided: false,
     });
 
-    return [player];
+    const updatePlayerPos = ({ x, y, collided}) => {
+        setPlayer(prev => ({
+            ...prev,
+            position: { x: (prev.position.x += x), y: (prev.position.y +=y)},
+            collided,
+        }))
+    }
+
+    const resetPlayer = useCallback(() => {
+            setPlayer({
+                position: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+                tetromino: randomTetromino().shape,
+                collided: false
+            })
+        }, [],
+    )
+
+    return [player, updatePlayerPos, resetPlayer];
 }
